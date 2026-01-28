c80db23155a36616c6472f0a967a9f43b4df59e6
PRAJWAL GHULE <prajwalghule2020@gmail.com>
2026-01-28 20:27:26 +0530

    04:auth ui

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,9 @@
+import { Card } from "@/components/ui/card";
+import { SignInView } from "@/modules/auth/ui/views/sign-in-view";
+
+
+export default function SignInPage() {
+  return (
+    <SignInView />
+  );
+}
